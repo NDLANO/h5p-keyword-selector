3a86f4d10a2f8377cb216d7b5bd515e060abb739
Oliver Tacke <o.tacke@posteo.de>
2023-07-08 17:57:20 +0200

    Add xAPI progressed for now

--- src/scripts/h5p-keyword-selector.js
+++ src/scripts/h5p-keyword-selector.js
@@ -105,6 +105,7 @@ export default class KeywordSelector extends H5P.EventDispatcher {
    */
   handleAnswered() {
     this.isAnswered = true;
+    this.triggerXAPIEvent('progressed'); // To notify parent and save state
   }
 
   /**
